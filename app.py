0334fac8fea1a390f070313742264a28fc600719
Andrea Fan <andrea.fan@ringus-solution.com>
2024-05-30 15:03:09 +0800

    Refactor image sending functionality and update image URLs for specific messages

--- app.py
+++ app.py
@@ -25,20 +25,6 @@ def callback():
         tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
         msg = json_data['events'][0]['message']['text']
         get_image(msg, tk)
-        
-        # if  msg == '椰咚' or msg == '禮志':
-        #     # 如果有圖片網址，回傳圖片
-        #     img_url = 'https://res.cloudinary.com/dbrf4i0rb/image/upload/v1717041883/Screenshot_2024-05-30_at_08.33.37_zi7k7l.png'
-        #     img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
-        #     line_bot_api.reply_message(tk,img_message)
-        # elif msg == '請鼓勵我':
-        #     img_url = 'https://res.cloudinary.com/dbrf4i0rb/image/upload/v1717049784/artworks-wO5B7Svz2sILlOYm-n5A8fA-t500x500_zgrrpi.jpg'
-        #     img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
-        #     line_bot_api.reply_message(tk,img_message)
-        # else:
-        #     # 如果 msg 不符合，回傳文字
-        #     text_message = TextSendMessage(text='找不到相關圖片')
-        #     line_bot_api.reply_message(tk,text_message)
     except:
         print(body)             # 如果發生錯誤，印出收到的內容
     return 'OK'                 # 驗證 Webhook 使用，不能省略
